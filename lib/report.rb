e8861b75c47b2f0bb7792bbece62ebbc1e5ed2ea
Alexander Lang <alex@skywalker.local>
2008-07-07 19:26:07 +0200

    now showing column names in first row in sql reports

--- lib/report.rb
+++ lib/report.rb
@@ -3,12 +3,22 @@ class Report < ActiveRecord::Base
   validates_presence_of :code, :name, :kind
   validates_uniqueness_of :name
   
-  
   def run
     if kind == 'sql'
-      ActiveRecord::Base.connection.select_all(code).map(&:values)
+      rows = ActiveRecord::Base.connection.select_all(code)
+      header(rows) + rows.map(&:values)
     else
       eval(code)
+    end
+  end
+  
+  private
+  
+  def header(rows)
+    if rows.first
+      [rows.first.map{|k,v| k}]
+    else
+      []
     end
   end
 end
--- spec/report_spec.rb
+++ spec/report_spec.rb
@@ -18,7 +18,13 @@ describe Report do
   
   it "should run the sql and return an array of arrays" do
     @report.kind = 'sql'
-    @report.code = 'SELECT 1, 2 UNION SELECT 2, 3'
-    @report.run.should == [['1', '2'], ['2', '3']]
+    @report.code = 'SELECT 1 AS col_1, 2 AS col_2 UNION SELECT 2 AS col_1, 3 AS col_3'
+    @report.run.should == [['col_1', 'col_2'], ['1', '2'], ['2', '3']]
+  end
+  
+  it "should return an empty array if no results" do
+    @report.kind = 'sql'
+    @report.code = 'SELECT 1 WHERE 0'
+    @report.run.should == []
   end
 end
